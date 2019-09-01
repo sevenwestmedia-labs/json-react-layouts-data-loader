e12076f0f6a9e8baf525553d71e19203869611fb
Jake Ginnivan <jake.ginnivan@wanews.com.au>
2019-09-01 16:37:46 +0800

    feat: Added API to get component data args from component

---
+++ src/get-data-args.ts
@@ -0,0 +1,14 @@
+import { getRegistrars, LayoutApi } from 'json-react-layouts'
+import { DataDefinition } from './DataLoading'
+
+export function getComponentDataArgs<Services>(
+    layout: LayoutApi<any, any, any, any>,
+    componentType: string,
+): DataDefinition<any, any, Services> | undefined {
+    const { componentRegistrar } = getRegistrars(layout)
+    const componentDataDefinition = componentRegistrar.get(componentType)
+
+    const dataDefinition = (componentDataDefinition as any).dataDefinition
+
+    return dataDefinition
+}
